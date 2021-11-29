d7f2bf7fb4322ab1d2a7d18beac16b6ab1c8b048
wiut00012025 <wiut00012025@gmail.com>
2021-11-29 21:48:27 +0500

    menu created

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def print_hi(name):
-    print(f'Hi, {name}')
-
-
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        # starting menu
+        print("<====== WIUT ======>")
+        print("1. Submit Coursework")
+        print("2. Submit MC")
+        print("3. View My MCs")
+        print("4. Quit")
+        print("")
 
+        menu_item = int(input("======> "))
+
+        if menu_item == 1:
+            print("Submit Coursework")
+        elif menu_item == 2:
+            print("Submit MC")
+        elif menu_item == 3:
+            print("View My MCs")
+        elif menu_item == 4:
+            break
+        else:
+            print("Enter Numbers 1-4!")
