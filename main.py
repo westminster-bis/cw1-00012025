8cca925df3e4be3ed29d4622c116d0591e8c0a1a
wiut00012025 <wiut00012025@gmail.com>
2021-11-29 22:12:23 +0500

    modules and deadlines added

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
+from datetime import datetime
+
+#function to convert string to date type
+def parse_date(str):
+    return datetime.strptime(str, '%d.%m.%Y')
+
+#function for printing beautiful lines
+def new_two_lines():
+    print("____________________________")
+    print("")
+    print("____________________________")
+
+#main function
 if __name__ == '__main__':
+    #dictionary that holds modules and deadlines
+    module_deadline = {
+        'CSF': parse_date("01.12.2021"),
+        'IMOB': parse_date("11.12.2021"),
+        'WT': parse_date("14.12.2021"),
+        'ISDS': parse_date("21.12.2021")
+    }
+
     while True:
         # starting menu
         print("<====== WIUT ======>")
-        print("1. Submit Coursework")
-        print("2. Submit MC")
-        print("3. View My MCs")
-        print("4. Quit")
-        print("")
+        print("1. View Modules and Deadlines")
+        print("2. Submit Coursework")
+        print("3. Submit MC")
+        print("4. View My MCs")
+        print("5. Quit")
+        new_two_lines()
 
         menu_item = int(input("======> "))
 
         if menu_item == 1:
-            print("Submit Coursework")
+            for module in module_deadline.keys():
+                print(module+": "+str(module_deadline.get(module)))
+            new_two_lines()
         elif menu_item == 2:
-            print("Submit MC")
+            print("Submit Coursework")
+            new_two_lines()
         elif menu_item == 3:
-            print("View My MCs")
+            print("Submit MC")
+            new_two_lines()
         elif menu_item == 4:
+            print("View My MCs")
+            new_two_lines()
+        elif menu_item == 5:
             break
         else:
             print("Enter Numbers 1-4!")
+            new_two_lines()
